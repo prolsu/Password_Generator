1b3726f0bcc1b5fece6a041f9972eba0988eb914
PedroAlburqueque <71682020+prolsu@users.noreply.github.com>
2020-10-14 17:08:15 -0400

    offers all options for a quicker result
    once user chooses to include all four options in one click, the number of digits is requested and password generated.

--- script.js
+++ script.js
@@ -4,61 +4,52 @@ var userSelection = "";
 var userInput;
 var pwLength;
 
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-  userSelection = "";
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 function generatePassword() {
     
     while (userSelection == ""){
-        alert("Please select at least one of the following options...");
-    
-        var smallCase = confirm("Add small case?");
-        var upperCase = confirm("Add upper case?");
-        var numberCase = confirm("Add numbers?");
-        var specialCase = confirm("Add special characters?");
-
-        console.log("User's smallcase choice: " + smallCase);
-        console.log("User's uppercase choice: " + upperCase);
-        console.log("User's numbers choice: " + numberCase);
-        console.log("User's special characters choice: " + specialCase);
 
         var smallCharset = "abcdefghijklmnopqrstuvwxyz";
         var upperCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         var numberCharset = "0123456789";
         var specialCharset = "! '()*+,-./:;<=>?@[\]^_`{|}~";
-           
-            if (smallCase == true) {
-                userSelection += smallCharset;
-            }      
-            if (upperCase == true) {
-                userSelection += upperCharset;
-            }
-            if (numberCase == true) {
-                userSelection += numberCharset;
-            }
-            if (specialCase == true) {
-                userSelection += specialCharset;
-            }
-            if (userSelection !== ""){
-                break;
-            }
-        
+        //offer user to include all option for a quicker result!
+        var allFour = confirm("Include all opctions: lowercase, uppercase, numbers, and special characters?");
+        if (allFour == true){
+            userSelection = smallCharset + upperCharset + numberCharset + specialCharset;
+            console.log(userSelection);
+            break;
+        }
+        alert("Please choose at least one of the following options to continue");
+        var smallCase = confirm("Add lowercase?");
+        var upperCase = confirm("Add uppercase?");
+        var numberCase = confirm("Add numbers?");
+        var specialCase = confirm("Add special characters?");
+
+        console.log("Lowercase included? " + smallCase);
+        console.log("Uppercase included? " + upperCase);
+        console.log("Numbers included? " + numberCase);
+        console.log("Special characters included? " + specialCase);
+
+        if (smallCase == true) {
+            userSelection += smallCharset;
+        }      
+        if (upperCase == true) {
+            userSelection += upperCharset;
+        }
+        if (numberCase == true) {
+            userSelection += numberCharset;
+        }
+        if (specialCase == true) {
+            userSelection += specialCharset;
+        }
+        if (userSelection == "") {
+            alert("At least one option must be selected!");
+        }
     }       
      
     while (userInput !== null || userInput !== "") {
 
-        userInput = prompt("How many characters do you need? Min:8 Max:128 ");
+        userInput = prompt("How many digits do you need? Min:8 Max:128 ");
         pwLength = parseInt(userInput); 
            
         if (pwLength/1 !== pwLength) {
@@ -66,23 +57,33 @@ function generatePassword() {
         }
                 
         else if (pwLength < 8 ) {
-            alert("Please enter a number greater than 8 and less than 128");
+            alert("Number must be greater than 8 and less than 128");
         }
         else if (pwLength > 128){
-            alert("Please enter a number lesser than 128 but greater than 8");
+            alert("Number must be less than 128 but greater than 8");
         }
             
         else {
             break;
         }
     }    
-        
-        charset = userSelection;
+   
         returnVal = "";
-    for (var i = 0, n = charset.length; i < pwLength; ++i) {
-        returnVal += charset.charAt(Math.floor(Math.random() * n));
+    for (var i = 0, n = userSelection.length; i < pwLength; ++i) {
+        returnVal += userSelection.charAt(Math.floor(Math.random() * n));
              
-    }console.log(pwLength); console.log(charset); console.log(returnVal);
+    }console.log(pwLength); console.log(userSelection); console.log(returnVal);
     return returnVal;
-    
 } 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+    userSelection = "";
+}
